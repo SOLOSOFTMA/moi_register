b78cf78cb613f501bf7a6925af316d6acf001a94
root <root@moi.register>
2020-06-11 00:29:34 +1300

    Update 11 June 2020

--- moi_register/moi_register/doctype/document/document.js
+++ moi_register/moi_register/doctype/document/document.js
@@ -3,21 +3,37 @@
 
 frappe.ui.form.on('Document', {
 	
+
 	
 	refresh: function(frm) {
-		 
 		
+		 frappe.call({
+            "method": "frappe.client.get",
+            args: {
+                   doctype: "Employee",
+                    filters: {
+                    user_id: frappe.session.user,
+                      }
+                    },
+                    callback: function(data) {
+						console.log(data.message("grade"));
+						if (data.message["grade"] == "I"){
+							cur_frm.set_df_property("assessment_section", "hidden", 0);
+						}else {
+							cur_frm.set_df_property("assessment_section", "hidden", 1);
+						}
+					}
+		 })
+//		console.log(frappe.user_roles)
+//		 if (frappe.user_has_roles in ("Document Approval")) {
+//			frappe.msgprint("Hello");
+//			cur_frm.set_df_property("department_approval_section", "hidden", 0);
+//		}else {
+//			
+//			cur_frm.set_df_property("department_approval_section", "hidden", 1);
+//		}
 		
-//		frm.set_query('document_to_employee', () => {
-//			return {
-//				filters: {
-//					acting_ceo:  ["=", 1]
-//				}
-//			}
-//		})
-		 
-		 
-		 
+			 
 		if (frm.doc.status == "Approved") {
             frm.add_custom_button(__('Assessment'), function() {
 					cur_frm.set_df_property("assessment_section", "hidden", 0);
@@ -34,27 +50,12 @@ frappe.ui.form.on('Document', {
 	 },
 	 internal_memo_type: function(frm){
 		 frm.set_value("subject",frm.doc.internal_memo_type);
-//		 frm.set_value("ref",frm.doc.name);
 		 
 		 if (frm.doc.internal_memo_type == "Request Transfer" || frm.doc.internal_memo_type == "Request for Procurement") {
 			  frm.set_df_property('ref',  'read_only', 1);
 		 } else {
 			 frm.set_df_property('ref',  'read_only', 0);
 		 }
-		
-				
-	
-		frappe.call({
-			"method": "frappe.client.get",
-				args: {
-					doctype: "Employee",
-					filters: {'acting_ceo': ["=", 1]},
-					},
-					callback: function (data) {
-					frm.set_value("document_to_employee",data.message["employee_name"])
-					}
-				})	  
-		 
 	 }
 });
 
--- moi_register/moi_register/doctype/document/document.py
+++ moi_register/moi_register/doctype/document/document.py
@@ -3,12 +3,24 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Document(Document):
     def on_submit(self):
         self.check_ref()    
+
+
+    def validate(self):
+        if not self.document_from:
+            self.get_document_from()
+        if self.hod_approval_status:
+            if not self.head_of_department_name:
+                self.get_department_approval()
+        
+        if self.director_of_ssd_status:
+            if not self.director_of_ssd_name:
+                self.get_director_of_ssd()
 
     def check_ref(self):
         if not self.ref:
@@ -16,3 +28,23 @@ class Document(Document):
 
     def get_name(self):
         return self.name
+
+
+    def get_document_from(self):
+        user = frappe.session.user
+        db_value = frappe.db.get_value("Employee", {"user_id": user}, ["name", "employee_name"], as_dict=1)   
+        self.document_from_employee = db_value.name
+        self.document_from = db_value.employee_name
+        
+    def get_department_approval(self):
+        user = frappe.session.user
+        db_value = frappe.db.get_value("Employee", {"user_id": user}, ["name", "employee_name"], as_dict=1)
+        self.head_of_department = db_value.name
+        self.head_of_department_name = db_value.employee_name
+        
+    def get_director_of_ssd(self):
+        user = frappe.session.user
+        db_value = frappe.db.get_value("Employee", {"user_id": user}, ["name", "employee_name"], as_dict=1)
+        self.director_of_ssd = db_value.name
+        self.director_of_ssd_name = db_value.employee_name
+    
